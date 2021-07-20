963dfea99c41c53f23574598e9b9bdf45f3177ba
LyubovDevialtovskaya <luba1297mail@gmail.com>
2021-07-20 19:52:29 +0300

    проверка апикея

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,5 +1,5 @@
 import {
-  movieList,triggerMode,addMovieToList,clearMoviesMarkup,createMarkup,createStyle,inputSearch,
+  movieList, triggerMode, addMovieToList, clearMoviesMarkup, createMarkup, createStyle, inputSearch,
 } from './dom.js';
 
 let siteUrl = null;
@@ -8,30 +8,30 @@ const debounce = (() => {
   let timer = 0;
 
   return (cb, ms) => {
-      clearTimeout(timer);
-      timer = setTimeout(cb, ms);
+    clearTimeout(timer);
+    timer = setTimeout(cb, ms);
   };
 })();
 const getData = (url) => fetch(url)
   .then((response) => response.json())
   .then((json) => {
-      if (!json || !json.Search) throw Error('Сервер вернул неправильный объект');
+    if (!json || !json.Search) throw Error('Сервер вернул неправильный объект');
 
-      return json.Search;
+    return json.Search;
   });
 
 const inputSearchHandler = (e) => {
   debounce(() => {
-      const searchString = e.target.value.trim();
+    const searchString = e.target.value.trim();
 
-      if (searchString && searchString.lenght > 3 && searchString !== searchLast) {
+    if (searchString && searchString.lenght > 3 && searchString !== searchLast) {
       if (!triggerMode) clearMoviesMarkup(movieList);
-          getData(`${siteUrl}?apikey=5be607b4=${searchString}`)
-              .then((movies) => movies.forEach((movie) => addMovieToList(movie)))
-              .catch((err) => console.log(err));
-      }
+      getData(`${siteUrl} ? apikey = 5be607b4 = ${searchString}`)
+        .then((movies) => movies.forEach((movie) => addMovieToList(movie)))
+        .catch((err) => console.log(err));
+    }
 
-      searchLast = searchString;
+    searchLast = searchString;
 
   }, 2000);
 };
