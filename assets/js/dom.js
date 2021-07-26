fce19881a5947058933e08413559b8aaf7aa29c1
LyubovDevialtovskaya <luba1297mail@gmail.com>
2021-07-26 18:54:55 +0300

    внесение правок

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -164,7 +164,7 @@ export const addMovieToList = (movie) => {
             class: 'movie__image',
             alt: movie.Title,
             title: movie.Title,
-            src: /^(http|https):\/\//i.test(movie.Poster) ? movie.Poster : 'assets/img/no-image.png'
+            src: /^(https):\/\//i.test(movie.Poster) ? movie.Poster : 'assets/img/no-image.png'
         },
         container: item
     });
